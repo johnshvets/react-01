a8e1770a9dee250d70fcd8115bafd6cb7a5db015
Ivan Shvets <johnshvets@gmail.com>
2020-09-26 13:14:26 +0300

    change header.jsx

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,18 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 import classes from "./Header.module.css";
 
-const Header = () => {
+const Header = (props) => {
   return (
     <header className={classes.header}>
-      <img src="https://static.dribbble.com/users/60166/screenshots/14000017/lion_logo_2x.jpg" />
+      <img
+        src="https://static.dribbble.com/users/60166/screenshots/14000017/lion_logo_2x.jpg"
+        className={classes.logo}
+      />
+
+      <div className={classes.loginBlock}>
+        {props.isAuth ? props.login : <NavLink to={"/login"}>Login</NavLink>}
+      </div>
     </header>
   );
 };
