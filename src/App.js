e6ae39512aef676417571350bed9c76ab766bb5f
Ivan Shvets <johnshvets@gmail.com>
2020-09-26 13:07:57 +0300

    change App.js

--- src/App.js
+++ src/App.js
@@ -1,29 +1,37 @@
 import React from "react";
 import "./App.css";
-import Header from "./components/Header/Header";
 import Navbar from "./components/Navbar/Navbar";
-import Profile from "./components/Profile/Profile";
 import { Route, BrowserRouter } from "react-router-dom";
 import Music from "./components/Music/Music";
 import News from "./components/News/News";
 import Settings from "./components/Settings/Settings";
 import DialogsContainer from "./components/Dialogs/DialogsContainer";
 import UsersContainer from "./components/Users/UsersContainer";
+import ProfileContainer from "./components/Profile/ProfileContainer";
+import HeaderContainer from "./components/Header/HeaderContainer";
+import Login from "./components/Login/Login";
 
 function App(props) {
   return (
     <BrowserRouter>
       <div className="app-wrapper">
-        <Header />
+        <HeaderContainer />
 
         <Navbar />
+
         <div className="app-wrapper-content">
-          <Route path="/profile" render={() => <Profile />} />
+          <Route path="/profile/:userId?" render={() => <ProfileContainer />} />
+
           <Route path="/dialogs" render={() => <DialogsContainer />} />
+
           <Route path="/users" render={() => <UsersContainer />} />
+
           <Route path="/news" render={() => <News />} />
+
           <Route path="/music" render={() => <Music />} />
+
           <Route path="/settings" render={() => <Settings />} />
+          <Route path="/login" render={() => <Login />} />
         </div>
       </div>
     </BrowserRouter>
