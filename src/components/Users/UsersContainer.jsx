d273abb0f78e7af34c62272ee7141c3cabd3199a
Ivan Shvets <johnshvets@gmail.com>
2020-09-26 13:18:50 +0300

    chnge UsersContainer.jsx

--- src/components/Users/UsersContainer.jsx
+++ src/components/Users/UsersContainer.jsx
@@ -1,29 +1,59 @@
 import React from "react";
 import { connect } from "react-redux";
-import UsersClass from "./UsersClass";
 import {
-  followAC,
-  unfollowAC,
-  setUsersAC,
-  setCurrentPageAC,
-  setTotalUserCountAC,
+  follow,
+  unfollow,
+  setCurrentPage,
+  getUsers,
 } from "../../redux/users-reducer";
+import Users from "./Users";
+import Loader from "../common/loader";
+import { compose } from "redux";
+import { withAuthRedirect } from "../hoc/WithAuthRedirect";
+
+class UsersContainer extends React.Component {
+  componentDidMount() {
+    this.props.getUsers(this.props.currentPage, this.props.pageSize);
+  }
+
+  onPageChange = (pageNumber) => {
+    this.props.getUsers(pageNumber, this.props.pageSize);
+  };
+
+  render() {
+    return (
+      <>
+        {this.props.isFetching ? <Loader /> : null}
+        <Users
+          totalUserCount={this.props.totalUserCount}
+          pageSize={this.props.pageSize}
+          currentPage={this.props.currentPage}
+          onPageChange={this.onPageChange}
+          users={this.props.users}
+          follow={this.props.follow}
+          unfollow={this.props.unfollow}
+          followingInProgress={this.props.followingInProgress}
+        />
+      </>
+    );
+  }
+}
 
 const mapStateToProps = (state) => ({
   users: state.usersPage.users,
   pageSize: state.usersPage.pageSize,
   totalUserCount: state.usersPage.totalUserCount,
   currentPage: state.usersPage.currentPage,
+  isFetching: state.usersPage.isFetching,
+  followingInProgress: state.usersPage.followingInProgress,
 });
 
-const mapDispatchToProps = (dispatch) => ({
-  follow: (userId) => dispatch(followAC(userId)),
-  unfollow: (userId) => dispatch(unfollowAC(userId)),
-  setUsers: (users) => dispatch(setUsersAC(users)),
-  setCurrentPage: (pageNumber) => dispatch(setCurrentPageAC(pageNumber)),
-  setTotalUserCount: (totalCount) => dispatch(setTotalUserCountAC(totalCount)),
-});
-
-const UsersContainer = connect(mapStateToProps, mapDispatchToProps)(UsersClass);
-
-export default UsersContainer;
+export default compose(
+  connect(mapStateToProps, {
+    follow,
+    unfollow,
+    setCurrentPage,
+    getUsers,
+  }),
+  withAuthRedirect
+)(UsersContainer);
