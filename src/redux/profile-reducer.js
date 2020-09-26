e64347a8f1ce8ee482e33c9255b1c3761b48f17c
Ivan Shvets <johnshvets@gmail.com>
2020-09-26 13:11:59 +0300

    change profile-reducer.js

--- src/redux/profile-reducer.js
+++ src/redux/profile-reducer.js
@@ -1,5 +1,8 @@
+import { userAPI } from "../api/api";
+
 const ADD_POST = "ADD-POST";
 const UPDATE_NEW_POST_TEXT = "UPDATE-NEW-POST-TEXT";
+const SET_USER_PROFILE = "SET_USER_PROFILE";
 
 const initialState = {
   posts: [
@@ -7,9 +10,10 @@ const initialState = {
     { id: 2, message: "It's my first post.", likesCount: 11 },
   ],
   newPostText: "Type here!",
+  profile: null,
 };
 
-export const profileReducer = (state = initialState, action) => {
+const profileReducer = (state = initialState, action) => {
   switch (action.type) {
     case ADD_POST:
       const newPost = {
@@ -23,17 +27,30 @@ export const profileReducer = (state = initialState, action) => {
         posts: [...state.posts, newPost],
         newPostText: "",
       };
+
     case UPDATE_NEW_POST_TEXT:
       return { ...state, newPostText: action.newText };
+
+    case SET_USER_PROFILE:
+      return { ...state, profile: action.profile };
     default:
       return state;
   }
 };
 
 export const addPostCreator = () => ({ type: ADD_POST });
+export const setUserProfile = (profile) => ({
+  type: SET_USER_PROFILE,
+  profile,
+});
 export const updateNewPostTextCreator = (text) => ({
   type: UPDATE_NEW_POST_TEXT,
   newText: text,
 });
 
+export const getUserProfile = (userId) => (dispatch) => {
+  userAPI
+    .getProfile(userId)
+    .then((response) => dispatch(setUserProfile(response.data)));
+};
 export default profileReducer;
