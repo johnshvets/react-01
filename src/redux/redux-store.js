d1cc51e23e52972de8631ba94d091dc9d8fd7d69
Ivan Shvets <johnshvets@gmail.com>
2020-09-26 13:11:22 +0300

    change redux-store.js

--- src/redux/redux-store.js
+++ src/redux/redux-store.js
@@ -1,15 +1,17 @@
 import profileReducer from "./profile-reducer";
 import dialogReducer from "./dialogs-reducer";
 import usersReducer from "./users-reducer";
+import authReducer from "./auth-reducer";
+import thunkMiddleware from "redux-thunk";
 
-const { createStore, combineReducers } = require("redux");
+const { createStore, combineReducers, applyMiddleware } = require("redux");
 
 const reducers = combineReducers({
   profilePage: profileReducer,
   dialogPage: dialogReducer,
   usersPage: usersReducer,
+  auth: authReducer,
 });
-
-const store = createStore(reducers);
+const store = createStore(reducers, applyMiddleware(thunkMiddleware));
 
 export default store;
