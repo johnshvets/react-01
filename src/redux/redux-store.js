9e0086f6c3e6ea9ac5ecd435844f6efbcb91a725
Ivan Shvets <johnshvets@gmail.com>
2020-10-05 22:45:17 +0300

    Create app-reducer.js

--- src/redux/redux-store.js
+++ src/redux/redux-store.js
@@ -4,6 +4,7 @@ import usersReducer from "./users-reducer";
 import authReducer from "./auth-reducer";
 import thunkMiddleware from "redux-thunk";
 import { reducer as formReducer } from "redux-form";
+import appReducer from "./app-reducer";
 
 const { createStore, combineReducers, applyMiddleware } = require("redux");
 
@@ -13,7 +14,9 @@ const reducers = combineReducers({
   usersPage: usersReducer,
   auth: authReducer,
   form: formReducer,
+  app: appReducer,
 });
+
 const store = createStore(reducers, applyMiddleware(thunkMiddleware));
 
 export default store;
