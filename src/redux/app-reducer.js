112d38a7a4bceec2d17b407cac7281c6c7d96af0
Ivan Shvets <johnshvets@gmail.com>
2020-11-01 15:08:04 +0200

    refactor app-reduer.js

--- src/redux/app-reducer.js
+++ src/redux/app-reducer.js
@@ -1,6 +1,6 @@
 import { getAuthUserData } from "./auth-reducer";
 
-const INITIALIZED_SUCCESS = "INITIALIZED_SUCCESS";
+const INITIALIZED_SUCCESS = "react-01/app/INITIALIZED_SUCCESS";
 
 const initialState = {
   initialized: false,
