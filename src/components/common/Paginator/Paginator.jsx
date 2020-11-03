c58495ff3447f772ff4f6c7d144a23bfbee9bb07
Ivan Shvets <johnshvets@gmail.com>
2020-11-03 15:56:25 +0200

    add portion page count to Paginator.jsx

--- src/components/common/Paginator/Paginator.jsx
+++ src/components/common/Paginator/Paginator.jsx
@@ -1,13 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import classes from "./Paginator.module.css";
 
 const Paginator = ({
-  totalUserCount,
+  totalItemCount,
   pageSize,
   currentPage,
   onPageChange,
+  portionSize = 5,
 } = {}) => {
-  const pageCount = Math.ceil(totalUserCount / pageSize);
+  const pageCount = Math.ceil(totalItemCount / pageSize);
 
   const pages = [];
 
@@ -15,18 +16,40 @@ const Paginator = ({
     pages.push(i);
   }
 
+  const portionCount = Math.ceil(pageCount / portionSize);
+  const [portionNumber, setPortionNumber] = useState(1);
+  const leftPortionPageNumber = (portionNumber - 1) * portionSize + 1;
+  const rightPortionPageNumber = portionNumber * portionSize;
+
   return (
     <div>
-      {pages.map((page) => {
-        return (
-          <button
-            className={currentPage === page && classes.selectedPage}
-            onClick={() => onPageChange(page)}
-          >
-            {page}
-          </button>
-        );
-      })}
+      {portionNumber > 1 && (
+        <button onClick={() => setPortionNumber(portionNumber - 1)}>
+          Prev
+        </button>
+      )}
+      {pages
+        .filter(
+          (page) =>
+            page >= leftPortionPageNumber && page <= rightPortionPageNumber
+        )
+        .map((page) => {
+          return (
+            <a
+              className={`${classes.page} ${
+                currentPage === page && classes.selectedPage
+              }`}
+              onClick={() => onPageChange(page)}
+            >
+              {page}
+            </a>
+          );
+        })}
+      {portionCount > portionNumber && (
+        <button onClick={() => setPortionNumber(portionNumber + 1)}>
+          Next
+        </button>
+      )}
     </div>
   );
 };
