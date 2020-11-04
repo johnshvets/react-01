f2fedb1a891c947947e461139f58faeda6b4cb67
Ivan Shvets <johnshvets@gmail.com>
2020-11-04 23:23:34 +0200

    add captchaUrl to Login.jsx

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -4,19 +4,23 @@ import { Redirect } from "react-router-dom";
 import { login } from "../../redux/auth-reducer";
 import ReduxLoginForm from "./LoginForm/LoginForm";
 
-const Login = ({ login, isAuth } = {}) => {
-  const onSubmit = ({ email, password, rememberMe } = {}) => {
-    login(email, password, rememberMe);
+const Login = ({ login, isAuth, captchaUrl } = {}) => {
+  const onSubmit = ({ email, password, rememberMe, captcha } = {}) => {
+    login(email, password, rememberMe, captcha);
   };
   return isAuth ? (
     <Redirect to={"/profile"} />
   ) : (
     <section>
-      <h1>Login</h1> <ReduxLoginForm onSubmit={onSubmit} />
+      <h1>Login</h1>{" "}
+      <ReduxLoginForm onSubmit={onSubmit} captchaUrl={captchaUrl} />
     </section>
   );
 };
 
-const mapStateToProps = (state) => ({ isAuth: state.auth.isAuth });
+const mapStateToProps = (state) => ({
+  isAuth: state.auth.isAuth,
+  captchaUrl: state.auth.captchaUrl,
+});
 
 export default connect(mapStateToProps, { login })(Login);
