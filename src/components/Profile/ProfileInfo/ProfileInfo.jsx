4ea7a54df19ad1db634d9a868414aca81ce64ee3
Ivan Shvets <johnshvets@gmail.com>
2020-11-04 18:42:45 +0200

    add ReduxProfileDataForm and ProfileData to ProfileInfo.jsx

--- src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -2,6 +2,9 @@ import React from "react";
 import Loader from "../../common/loader";
 import ProfileStatus from "./ProfileStatus/ProfileStatus";
 import userPic from "../../../images/user.svg";
+import ProfileData from "./ProfileData/ProfileData";
+import ReduxProfileDataForm from "./ProfileData/ProfileDataForm";
+import { useState } from "react";
 
 const ProfileInfo = ({
   isOwner,
@@ -9,11 +12,18 @@ const ProfileInfo = ({
   status,
   updateProfileStatus,
   savePhoto,
+  saveProfileData,
 } = {}) => {
+  const [editMode, setEditMode] = useState(false);
+
   const onMainPicChange = (e) => {
     if (e.target.files.length) {
       savePhoto(e.target.files[0]);
     }
+  };
+
+  const onSubmit = (formData) => {
+    saveProfileData(formData).then(() => setEditMode(false));
   };
 
   return !profile ? (
@@ -21,7 +31,7 @@ const ProfileInfo = ({
   ) : (
     <section>
       <div>
-        <h1>Name</h1>
+        <h1>{profile.fullName}</h1>
         <img src={profile.photos.large || userPic} width="250" />
         {isOwner && <input type={"file"} onChange={onMainPicChange} />}
         <ProfileStatus
@@ -29,6 +39,19 @@ const ProfileInfo = ({
           updateProfileStatus={updateProfileStatus}
         />
       </div>
+      {editMode ? (
+        <ReduxProfileDataForm
+          initialValues={profile}
+          profile={profile}
+          onSubmit={onSubmit}
+        />
+      ) : (
+        <ProfileData
+          profile={profile}
+          isOwner={isOwner}
+          activateEditMode={() => setEditMode(true)}
+        />
+      )}
     </section>
   );
 };
